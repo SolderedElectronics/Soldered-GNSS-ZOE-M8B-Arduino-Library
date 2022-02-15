7d7a5616c646da981b4875ace53282394303be27
Main Push Robot <robot@example.com>
2022-02-15 11:54:06 +0000

    Committing dev changes

---
+++ src/GNSS-ZOE-M8B-SOLDERED.cpp
@@ -0,0 +1,12 @@
+/**
+ **************************************************
+ *
+ * @file        HX711.cpp
+ * @brief       Intentionally blank
+ *
+ *
+ *
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+#include "GNSS-ZOE-M8B-SOLDERED.h"
