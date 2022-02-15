c8cebfeefc0d1fc30937b608df7b0f14c07e27e6
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-02-15 12:17:16 +0100

    Initial commit

---
+++ src/GNSS-ZOE-M8B-SOLDERED.h
@@ -0,0 +1,12 @@
+/**
+ **************************************************
+ *
+ * @file        GNSS-ZOE-M8B-SOLDERED.h
+ * @brief       Arduino library for GNSS ZOE M8B
+ *
+ *
+ *
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
